3ccc50495ce7fc5e95d059c1b27a95b6173f01b8
Tyler Cavanaugh <149531030+Tyler-Cav@users.noreply.github.com>
2026-05-29 16:15:00 -0400

    - Fix viewport height issue. - Add text input blink bar. - animate welcome text

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,4 +6,31 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {}
+export class HomePage implements OnInit{
+
+  public welcomeString: string = "Welcome..."
+  public visualText = signal('');
+  public fakeInputLine: string = "|"
+  public animatedText: string[] = []
+
+  ngOnInit(): void {
+
+    this.animateText();
+
+  }
+
+  animateText() {
+    for (let i = 0; i < this.welcomeString.length; i++) {
+      if (this.visualText().length < this.welcomeString.length) {
+        this.buildText(i);
+      }
+    }
+  }
+
+  buildText(i: number) {
+    setTimeout(() => {
+      this.visualText.update(prev => prev + this.welcomeString[i]);
+    }, i * 200);
+  }
+
+}
