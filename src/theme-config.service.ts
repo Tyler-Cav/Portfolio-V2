b39d2acb72dd3b49ef5237c68b4b624d65eacf0d
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2026-05-28 23:31:27 -0400

    update dark theme to default in the :root

--- src/theme-config.service.ts
+++ src/theme-config.service.ts
@@ -2,31 +2,25 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class ThemeService {
-
-  private currentTheme = signal(localStorage.getItem('themeOption'))
+  private currentTheme = signal(localStorage.getItem('themeOption'));
 
   toggleThemeOnClick() {
-
-    if (this.currentTheme() === 'light-theme') {
-      localStorage.setItem('themeOption', 'dark-theme');
-      this.currentTheme.set('dark-theme');
-      document.body.classList.add('dark-theme');
-    } else {
-      this.currentTheme.set('light-theme');
+    if (this.currentTheme() === 'dark-theme') {
       localStorage.setItem('themeOption', 'light-theme');
-      document.body.classList.remove('dark-theme');
+      this.currentTheme.set('light-theme');
+      document.body.classList.add('light-theme');
+    } else {
+      this.currentTheme.set('dark-theme');
+      localStorage.setItem('themeOption', 'dark-theme');
+      document.body.classList.remove('light-theme');
     }
-
   }
 
   themeInitCheck() {
-
-    if (this.currentTheme() === 'dark-theme') {
-      document.body.classList.add('dark-theme');
+    if (this.currentTheme() === 'light-theme') {
+      document.body.classList.add('light-theme');
     } else {
-      document.body.classList.remove('dark-theme');
+      document.body.classList.remove('light-theme');
     }
-
   }
-
 }
