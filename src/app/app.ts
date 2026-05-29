db65f87c00980efbadfba92d8c35c4509213d87e
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2026-05-28 21:24:52 -0400

    see previous message

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,20 @@
 import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeButton } from './header/theme-button/theme-button';
-import { ThemeService } from '../theme-config.service'
+import { ThemeService } from '../theme-config.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ThemeButton],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App implements OnInit {
-
   constructor(private themeService: ThemeService) {}
 
   protected readonly title = signal('Portfolio-V2');
 
   ngOnInit(): void {
-
     this.themeService.themeInitCheck();
-
   }
-
 }
