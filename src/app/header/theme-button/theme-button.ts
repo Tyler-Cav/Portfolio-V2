cb66234ca4d1bb7dc872ecaa3b999fc124ce2477
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2026-05-31 16:51:31 -0400

    - Add lightBulb frame svg's. - Add mouseup and mousedown logic to animate the light bulb and change the theme config.

--- src/app/header/theme-button/theme-button.ts
+++ src/app/header/theme-button/theme-button.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, WritableSignal, inject } from '@angular/core';
+import { Component, OnInit, WritableSignal, inject, viewChild, ElementRef, signal } from '@angular/core';
 import { ThemeService } from '../../../theme-config.service';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-theme-button',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+  ],
   templateUrl: './theme-button.html',
   styleUrl: './theme-button.css',
 })
@@ -13,17 +16,44 @@ export class ThemeButton implements OnInit {
 
   public themeSignal!: WritableSignal<string | null>;
 
+  public bulbSource = signal<string>("");
+
+  public bulbSources = {
+    staticLight: 'Test-LightBulb-Frame1.svg',
+    staticDark: 'Test-Darkbulb-Frame-1.svg',
+    animatedLight: 'Test-Lightbulb-Frame-2.svg',
+    animatedDark: 'Test-Darkbulb-Frame-2.svg'
+  };
+
   themeService = inject(ThemeService);
 
   ngOnInit() {
 
     this.themeSignal = this.themeService.getCurrentTheme();
-    this.lightMode = this.themeSignal() === 'light-theme';
+    if (this.lightMode) {
+      this.themeSignal() === 'light-theme';
+      this.bulbSource.set(this.bulbSources.staticLight);
+    } else {
+      this.bulbSource.set(this.bulbSources.staticDark);
+    }
   }
 
-  toggleTheme() {
-    this.themeService.toggleThemeOnClick();
-    this.themeSignal = this.themeService.getCurrentTheme();
-    this.lightMode = this.themeSignal() === 'light-theme';
+  changeBulb(event: Event) {
+    if (event.type === 'mouseup') {
+      this.themeService.toggleThemeOnClick();
+      this.themeSignal = this.themeService.getCurrentTheme();
+      this.lightMode = this.themeSignal() === 'light-theme';
+      if (this.lightMode) {
+        this.bulbSource.set(this.bulbSources.staticLight);
+      } else {
+        this.bulbSource.set(this.bulbSources.staticDark);
+      }
+    } else if (event.type === 'mousedown') {
+      if (this.lightMode) {
+        this.bulbSource.set(this.bulbSources.animatedLight);
+      } else {
+        this.bulbSource.set(this.bulbSources.animatedDark);
+      }
+    }
   }
 }
