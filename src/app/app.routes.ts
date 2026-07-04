6d6cbb2205cb5e1ec573ad325710aa7fbc6510b7
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2026-07-04 10:39:50 -0400

    - add keyboard path to routes. - Create sticky side buttons for content keyboard page nav. Need to implement functionality. - Start implementing scroll behavior by piece of keyboard content. - Removed global css properties not needed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { StyleGuide } from './For-Dev-Only/style-guide/style-guide';
 import { HomePage } from './home-page/home-page';
+import { KeyboardCollectionPage } from './keyboard-collection-page/keyboard-collection-page'
 
 export const routes: Routes = [
   {
@@ -16,4 +17,8 @@ export const routes: Routes = [
     path: 'style-guide',
     component: StyleGuide,
   },
+  {
+    path: 'keyboard-collection',
+    component: KeyboardCollectionPage
+  }
 ];
